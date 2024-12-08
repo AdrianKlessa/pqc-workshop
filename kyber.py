b7efe5d4a50970b196f245ec8861c47bb7bdb0ae
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2024-12-08 17:40:01 +0100

    Change some kyber member methods to functions, add typing

--- kyber.py
+++ kyber.py
@@ -1,10 +1,52 @@
+from typing import Sequence
+
 from polynomial_helper import Polynomial
 import numpy as np
 from random import randrange
 from decimal import Decimal, ROUND_HALF_UP
 
 
-class KyberSystem():
+def get_nearest_integer_to_half(q: int) -> int:
+    """
+    Custom function for finding the nearest integer to q/2, rounding ties up as defined in the Kyber specification
+    :param q:
+    :return: Number closest to q/2, with ties rounded up
+    """
+    return int(Decimal(q / 2).quantize(Decimal('1'), rounding=ROUND_HALF_UP))
+
+
+def decimal_to_binary(n: int) -> str:
+    """
+    Custom function for converting a decimal number to a binary string without the 0b prefix
+    :param n: Decimal number to convert
+    :return: Binary string representation of n
+    """
+    return bin(n).replace("0b", "")
+
+
+def get_coeff_from_eta(eta) -> int:
+    """
+    Get a random coefficient from the Centered Binomial Distribution defined by parameter eta
+    :param eta: Value of eta, as defined in the Kyber specification
+    :return: A single number from the eta Centered Binomial Distribution
+    """
+    random_bits_1 = np.random.randint(0, 2, size=eta)
+    random_bits_2 = np.random.randint(0, 2, size=eta)
+    return abs(random_bits_1.sum() - random_bits_2.sum())
+
+
+def generate_polynomial(n, q, eta) -> Sequence[int]:
+    """
+    Generate a polynomial with n coefficients from the Centered Binomial Distribution defined by parameter eta
+    :param n: Number of coefficients to generate
+    :param q: Modulus for coefficients
+    :param eta: Parameter for the Centered Binomial Distribution
+    :return:
+    """
+    return [get_coeff_from_eta(eta) % q for _ in range(n)]
+
+
+class KyberSystem:
     n: int
     k: int
     q: int
@@ -55,7 +97,7 @@ class KyberSystem():
         # Generate secret key
         s_polynomials = []
         for i in range(self.k):
-            s_polynomials.append(Polynomial(self.generate_polynomial(self.n, self.q, self.eta_1)))
+            s_polynomials.append(Polynomial(generate_polynomial(self.n, self.q, self.eta_1)))
         s = np.array(s_polynomials, ndmin=2).T
 
         # Generate matrix A for the public key
@@ -73,7 +115,7 @@ class KyberSystem():
         # Generate e
         e_polynomials = []
         for i in range(self.k):
-            e_polynomials.append(Polynomial(self.generate_polynomial(self.n, self.q, self.eta_1)))
+            e_polynomials.append(Polynomial(generate_polynomial(self.n, self.q, self.eta_1)))
         e = np.array(e_polynomials, ndmin=2).T
 
         # Calculate t for the public key
@@ -92,17 +134,17 @@ class KyberSystem():
         #r
         r_polynomials = []
         for i in range(self.k):
-            r_polynomials.append(Polynomial(self.generate_polynomial(self.n, self.q, self.eta_1)))
+            r_polynomials.append(Polynomial(generate_polynomial(self.n, self.q, self.eta_1)))
         r = np.array(r_polynomials, ndmin=2).T
 
         #e1
         e1_polynomials = []
         for i in range(self.k):
-            e1_polynomials.append(Polynomial(self.generate_polynomial(self.n, self.q, self.eta_2)))
+            e1_polynomials.append(Polynomial(generate_polynomial(self.n, self.q, self.eta_2)))
         e1 = np.array(e1_polynomials, ndmin=2).T
 
         #e2
-        e2 = Polynomial(self.generate_polynomial(self.n, self.q, self.eta_2))
+        e2 = Polynomial(generate_polynomial(self.n, self.q, self.eta_2))
 
         m_polynomial = self.encode_message_to_polynomial(m)
         u = np.dot(A.T, r) + e1
@@ -130,14 +172,14 @@ class KyberSystem():
         converted_to_decimal = int(denoised_string.rstrip("0")[::-1], 2)
         return converted_to_decimal
 
-    def denoise(self, coefficient_list):
+    def denoise(self, coefficient_list: Sequence[int]) -> Sequence[int]:
         """
         Returns 0 for every coefficient in coefficient_list closer to q or 0, 1 for every coefficient closer to q/2
         :param coefficient_list: Coefficents of the noisy message
         :return: Denoised coefficients
         """
         q = self.q
-        q_half = self.get_nearest_integer_to_half(self.q)
+        q_half = get_nearest_integer_to_half(self.q)
         denoised_coefficient_list = []
         for coeff in coefficient_list:
             dist_q = abs(coeff - q)
@@ -153,52 +195,16 @@ class KyberSystem():
             denoised_coefficient_list[i] = int(denoised_coefficient_list[i] / q_half)
         return denoised_coefficient_list
 
-    def get_nearest_integer_to_half(self, q: int) -> int:
-        """
-        Custom function for finding the nearest integer to q/2, rounding ties up as defined in the Kyber specification
-        :param q:
-        :return: Number closest to q/2, with ties rounded up
-        """
-        return int(Decimal(q / 2).quantize(Decimal('1'), rounding=ROUND_HALF_UP))
-
     def encode_message_to_polynomial(self, m: int) -> Polynomial:
         """
         Encode a given message (int) into a polynomial with binary coefficients
         :param m: Message to encode
         :return: Encoded message (Polynomial)
         """
-        m_binary_string = self.decimalToBinary(m)
-        q_half = self.get_nearest_integer_to_half(self.q)
+        m_binary_string = decimal_to_binary(m)
+        q_half = get_nearest_integer_to_half(self.q)
 
         # Reverse to prevent truncation of messages ending with 0
         coeffs = list(reversed([int(i) * q_half for i in m_binary_string]))
         poly = Polynomial(coeffs)
         return poly
-
-    def decimalToBinary(self, n):
-        """
-        Custom function for converting a decimal number to a binary string without the 0b prefix
-        :param n:
-        :return:
-        """
-        return bin(n).replace("0b", "")
-
-    def get_coeff_from_eta(self, eta):
-        """
-        Get a random coefficient from the Centered Binomial Distribution defined by parameter eta
-        :param eta: Value of eta, as defined in the Kyber specification
-        :return: A single number from the eta Centered Binomial Distribution
-        """
-        random_bits_1 = np.random.randint(0, 2, size=eta)
-        random_bits_2 = np.random.randint(0, 2, size=eta)
-        return abs(random_bits_1.sum() - random_bits_2.sum())
-
-    def generate_polynomial(self, n, q, eta):
-        """
-        Generate a polynomial with n coefficients from the Centered Binomial Distribution defined by parameter eta
-        :param n: Number of coefficients to generate
-        :param q: Modulus for coefficients
-        :param eta: Parameter for the Centered Binomial Distribution
-        :return:
-        """
-        return [self.get_coeff_from_eta(eta) % q for _ in range(n)]
