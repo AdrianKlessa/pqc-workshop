6abe7d2ac210701157179016958d27cbe107ceb7
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2025-01-04 14:17:02 +0100

    Spelling

--- ntru.py
+++ ntru.py
@@ -91,7 +91,7 @@ class NTRU:
 def encode_message(message: int)->Polynomial:
     binary = str(bin(message))[2:]
     binary = [int(i) for i in binary]
-    # Inversing the binary strings to prevent truncation of leading coefficients equal to 0
+    # Reversing the binary strings to prevent truncation of leading coefficients equal to 0
     binary = binary[::-1]
     return Polynomial(binary)
 
